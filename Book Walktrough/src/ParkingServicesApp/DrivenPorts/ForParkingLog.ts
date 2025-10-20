1dd26262e3e80f67acafe64b298ceb48835ba333
Olarn U. <olarn.u@odds.team>
2025-10-20 14:52:30 +0700

    Tidy. Add space in ForParkingLog class.

--- Book Walktrough/src/ParkingServicesApp/DrivenPorts/ForParkingLog.ts
+++ Book Walktrough/src/ParkingServicesApp/DrivenPorts/ForParkingLog.ts
@@ -1,4 +1,5 @@
 import { ParkingTransaction } from "ParkingServicesApp/ParkingServices/ParkingServices"
+
 export interface ForParkingLog {
   write(parkingTransaction: ParkingTransaction): string
 }
